10b6d19a08f5dafedb0b1080c4a9a2867c87f00b
Vencixa2 <157669079+Vencixa2@users.noreply.github.com>
2024-05-16 10:45:25 -0500

    3

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    window.location.href = "form-submitted.html";
-});
+const appointmentForm = document.getElementById("form");
+const shadow = document.getElementById("shadow");
+
+function toggleForm() {
+    appointmentForm.classList.toggle("active");
+    shadow.classList.toggle("active");
+}
