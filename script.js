3cd88c58a0c514320883e3794fc526ae42e057c6
Vencixa2 <157669079+Vencixa2@users.noreply.github.com>
2025-05-15 16:40:58 +0300

    da

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const appointmentForm = document.getElementById("form");
 const shadow = document.getElementById("shadow");
+const header = document.getElementById("header");
+const headerText = document.getElementById("header-text");
 
 function toggleForm() {
     appointmentForm.classList.toggle("active");
     shadow.classList.toggle("active");
 }
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+        headerText.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+        headerText.classList.remove("scrolled");
+    }
+})
