fa4c64542c69fd8d3a62971a2497b220bb9191fe
Vencixa2 <157669079+Vencixa2@users.noreply.github.com>
2025-05-23 23:51:35 +0300

    g

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-const appointmentForm = document.getElementById("form");
-const shadow = document.getElementById("shadow");
 const header = document.getElementById("header");
-const headerText = document.getElementById("header-text");
-
-function toggleForm() {
-    appointmentForm.classList.toggle("active");
-    shadow.classList.toggle("active");
-}
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
         header.classList.add("scrolled");
-        headerText.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
-        headerText.classList.remove("scrolled");
     }
-})
+});
+
--- service-script.js
+++ service-script.js
@@ -1,7 +1,8 @@
-const appointmentForm = document.getElementById("form");
-const shadow = document.getElementById("shadow");
-
-function toggleForm() {
-    appointmentForm.classList.toggle("active");
-    shadow.classList.toggle("active");
-}
+document.getElementById("side-form").addEventListener("submit", function(e) {
+    const service = document.getElementById("service");
+    if (service.value === "") {
+        e.preventDefault();
+        alert("Please choose a service.");
+        service.focus();
+    }
+});
